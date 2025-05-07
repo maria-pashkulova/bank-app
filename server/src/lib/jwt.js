0741234705a899bca5557cddf5e3f1d254b2090e
MariaIP28 <mariapashkulova28@gmail.com>
2025-05-07 11:30:45 +0300

    Add jsonwebtoken library and extend it

---
+++ server/src/lib/jwt.js
@@ -0,0 +1,40 @@
+//Make jsonwebtoken library work with promises instead of callbacks for asynchronous operations
+//Extend jsonwebtoken library
+
+import jsonwebtoken from "jsonwebtoken";
+
+const sign = (payload, secret, options) => {
+  //new Promise (executor function - gets 2 params resolve и reject)
+  const promise = new Promise((resolve, reject) => {
+    jsonwebtoken.sign(payload, secret, options, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(token);
+    });
+  });
+
+  return promise;
+};
+
+const verify = (token, secret) => {
+  const promise = new Promise((resolve, reject) => {
+    jsonwebtoken.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(decodedToken);
+    });
+  });
+
+  return promise;
+};
+
+const jwtPromises = {
+  sign,
+  verify,
+};
+
+export default jwtPromises;
